1e1574a64769abef1f7da4dc343f765ded87a1a6
aNGEl <eldar@nazmiev.ml>
2019-08-01 16:05:57 +0300

    555_Ready

--- PART_12/BEE_HIVE/Form1.Designer.cs
+++ PART_12/BEE_HIVE/Form1.Designer.cs
@@ -29,11 +29,259 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnTsStartSimulation = new System.Windows.Forms.ToolStripButton();
+            this.btnTsReset = new System.Windows.Forms.ToolStripButton();
+            this.lblTsStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BeesLbl = new System.Windows.Forms.Label();
+            this.FlowersLbl = new System.Windows.Forms.Label();
+            this.HoneyInHiveLbl = new System.Windows.Forms.Label();
+            this.NectarInFlowersLbl = new System.Windows.Forms.Label();
+            this.FramesRunLbl = new System.Windows.Forms.Label();
+            this.FramesRateLbl = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnTsStartSimulation,
+            this.btnTsReset});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblTsStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // btnTsStartSimulation
+            // 
+            this.btnTsStartSimulation.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnTsStartSimulation.Image = ((System.Drawing.Image)(resources.GetObject("btnTsStartSimulation.Image")));
+            this.btnTsStartSimulation.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnTsStartSimulation.Name = "btnTsStartSimulation";
+            this.btnTsStartSimulation.Size = new System.Drawing.Size(95, 22);
+            this.btnTsStartSimulation.Text = "Start Simulation";
+            // 
+            // btnTsReset
+            // 
+            this.btnTsReset.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnTsReset.Image = ((System.Drawing.Image)(resources.GetObject("btnTsReset.Image")));
+            this.btnTsReset.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnTsReset.Name = "btnTsReset";
+            this.btnTsReset.Size = new System.Drawing.Size(39, 22);
+            this.btnTsReset.Text = "Reset";
+            // 
+            // lblTsStatus
+            // 
+            this.lblTsStatus.Name = "lblTsStatus";
+            this.lblTsStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.BeesLbl, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.FlowersLbl, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.HoneyInHiveLbl, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.NectarInFlowersLbl, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.FramesRunLbl, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.FramesRateLbl, 1, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(260, 118);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 155);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 82);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "# Bees";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "# Flowers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total honey in the hive";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total nectar in the field";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Frames run";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Frame rate";
+            // 
+            // BeesLbl
+            // 
+            this.BeesLbl.AutoSize = true;
+            this.BeesLbl.Location = new System.Drawing.Point(133, 0);
+            this.BeesLbl.Name = "BeesLbl";
+            this.BeesLbl.Size = new System.Drawing.Size(0, 13);
+            this.BeesLbl.TabIndex = 6;
+            // 
+            // FlowersLbl
+            // 
+            this.FlowersLbl.AutoSize = true;
+            this.FlowersLbl.Location = new System.Drawing.Point(133, 19);
+            this.FlowersLbl.Name = "FlowersLbl";
+            this.FlowersLbl.Size = new System.Drawing.Size(0, 13);
+            this.FlowersLbl.TabIndex = 7;
+            // 
+            // HoneyInHiveLbl
+            // 
+            this.HoneyInHiveLbl.AutoSize = true;
+            this.HoneyInHiveLbl.Location = new System.Drawing.Point(133, 38);
+            this.HoneyInHiveLbl.Name = "HoneyInHiveLbl";
+            this.HoneyInHiveLbl.Size = new System.Drawing.Size(0, 13);
+            this.HoneyInHiveLbl.TabIndex = 8;
+            // 
+            // NectarInFlowersLbl
+            // 
+            this.NectarInFlowersLbl.AutoSize = true;
+            this.NectarInFlowersLbl.Location = new System.Drawing.Point(133, 58);
+            this.NectarInFlowersLbl.Name = "NectarInFlowersLbl";
+            this.NectarInFlowersLbl.Size = new System.Drawing.Size(0, 13);
+            this.NectarInFlowersLbl.TabIndex = 9;
+            // 
+            // FramesRunLbl
+            // 
+            this.FramesRunLbl.AutoSize = true;
+            this.FramesRunLbl.Location = new System.Drawing.Point(133, 78);
+            this.FramesRunLbl.Name = "FramesRunLbl";
+            this.FramesRunLbl.Size = new System.Drawing.Size(0, 13);
+            this.FramesRunLbl.TabIndex = 10;
+            // 
+            // FramesRateLbl
+            // 
+            this.FramesRateLbl.AutoSize = true;
+            this.FramesRateLbl.Location = new System.Drawing.Point(133, 98);
+            this.FramesRateLbl.Name = "FramesRateLbl";
+            this.FramesRateLbl.Size = new System.Drawing.Size(0, 13);
+            this.FramesRateLbl.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnTsStartSimulation;
+        private System.Windows.Forms.ToolStripButton btnTsReset;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblTsStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label BeesLbl;
+        private System.Windows.Forms.Label FlowersLbl;
+        private System.Windows.Forms.Label HoneyInHiveLbl;
+        private System.Windows.Forms.Label NectarInFlowersLbl;
+        private System.Windows.Forms.Label FramesRunLbl;
+        private System.Windows.Forms.Label FramesRateLbl;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- PART_12/BEE_HIVE/Form1.cs
+++ PART_12/BEE_HIVE/Form1.cs
@@ -12,9 +12,30 @@ namespace BEE_HIVE
 {
     public partial class Form1 : Form
     {
+        private World world;
+        private int framesRun;
         public Form1()
         {
             InitializeComponent();
+            world = new World();
+        }
+
+        private void UpdateStats(TimeSpan frameDuration)
+        {
+            BeesLbl.Text = world.Bees.Count.ToString();
+            FlowersLbl.Text = world.Flowers.Count.ToString();
+            HoneyInHiveLbl.Text = String.Format("{0:f3}", world.Hive.Honey);
+
+            double nectar = 0;
+            foreach (Flower flower in world.Flowers)
+                nectar += flower.Nectar;
+            NectarInFlowersLbl.Text = String.Format("{0:f3}", nectar);
+            FramesRunLbl.Text = framesRun.ToString();
+            double milliSeconds = frameDuration.TotalMilliseconds;
+            if (milliSeconds != 0.0)
+                FramesRateLbl.Text = string.Format("{0:f0} ({1:f1}ms)", 1000 / milliSeconds, milliSeconds);
+            else
+                FramesRateLbl.Text = "N/A";
         }
     }
 }
