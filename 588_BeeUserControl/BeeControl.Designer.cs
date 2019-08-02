4f36091afd6cac05d9af5e3766c674755ba7dda4
aNGEl <eldar@nazmiev.ml>
2019-08-02 11:26:31 +0300

    590_Ready

---
+++ 588_BeeUserControl/BeeControl.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace _588_BeeUserControl
+{
+    partial class BeeControl
+    {
+        /// <summary> 
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.animationTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // animationTimer
+            // 
+            this.animationTimer.Enabled = true;
+            this.animationTimer.Interval = 150;
+            this.animationTimer.Tick += new System.EventHandler(this.animationTimer_Tick);
+            // 
+            // BeeControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "BeeControl";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer animationTimer;
+    }
+}
---
+++ 588_BeeUserControl/BeeControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _588_BeeUserControl
+{
+    public partial class BeeControl : UserControl
+    {
+        int cell = 0;
+        public BeeControl()
+        {
+            InitializeComponent();
+            BackColor = System.Drawing.Color.Transparent;
+            BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void animationTimer_Tick(object sender, EventArgs e)
+        {
+            cell++;
+            switch (cell)
+            {
+                case 1: BackgroundImage = Properties.Resources.Bee_animation_1; break;
+                case 2: BackgroundImage = Properties.Resources.Bee_animation_2; break;
+                case 3: BackgroundImage = Properties.Resources.Bee_animation_3; break;
+                case 4: BackgroundImage = Properties.Resources.Bee_animation_4; break;
+                case 5: BackgroundImage = Properties.Resources.Bee_animation_3; break;
+                default: BackgroundImage = Properties.Resources.Bee_animation_2;
+                    cell = 0; break;
+            }
+        }
+    }
+}
