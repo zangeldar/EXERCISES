280dcc81dcb1c9dc171a73dfc23b8e63455bd5ef
zangeldar <zangeldar@gmail.com>
2019-08-05 12:26:23 +0300

    614_ready

---
+++ PART13/613_Scale/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace _613_Scale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zoomer1 = new _613_Scale.Zoomer();
+            this.SuspendLayout();
+            // 
+            // zoomer1
+            // 
+            this.zoomer1.Location = new System.Drawing.Point(12, 12);
+            this.zoomer1.Name = "zoomer1";
+            this.zoomer1.Size = new System.Drawing.Size(304, 305);
+            this.zoomer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 320);
+            this.Controls.Add(this.zoomer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Zoomer zoomer1;
+    }
+}
+
