cddb6034fa0aab01883efc8a932590d4511d7488
zangeldar <zangeldar@gmail.com>
2019-08-07 15:25:12 +0300

    658_Done

---
+++ Part_14/658_Robust/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _658_Robust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input birthday date:");
+            string birthday = Console.ReadLine();
+            Console.WriteLine("Input height inches:");
+            string height = Console.ReadLine();
+            RobustGuy guy = new RobustGuy(birthday, height);
+            Console.WriteLine(guy.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Part_14/658_Robust/RobustGuy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _658_Robust
+{
+    class RobustGuy
+    {
+        public DateTime? Birthday { get; private set; }
+        public int? Height { get; private set; }
+        public RobustGuy(string birthday, string height)
+        {
+            DateTime tempDate;
+            if (DateTime.TryParse(birthday, out tempDate))
+                Birthday = tempDate;
+            else
+                Birthday = null;
+
+            int tempInt;
+            if (int.TryParse(height, out tempInt))
+                Height = tempInt;
+            else
+                Height = null;
+        }
+
+        public override string ToString()
+        {
+            string description;
+            if (Birthday != null)
+                description = "I was born at " + Birthday.Value.ToLongDateString();
+            else
+                description = "I don't know when I was born";
+
+            if (Height != null)
+                description += ", I have " + Height + " inches height.";
+            else
+                description += ", and I don't know my height.";
+            
+            return description;
+        }
+    }
+}
