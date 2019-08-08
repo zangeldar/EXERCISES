96e8d5721bfd2bad35fcfc59c198b87c309b6385
zangeldar <zangeldar@gmail.com>
2019-08-08 10:18:36 +0300

    665_DONE

---
+++ Part_14/665_HOLOD/Margin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _665_HOLOD_Upside
+{
+    public static class Margin
+    {
+        public static void SendIt(this string s)
+        {
+            Console.Write(s);
+        }
+        public static string ToPrice(this int n)
+        {
+            if (n == 1)
+                return "a bucks ";
+//  может поменять с другим блоком else
+            else
+                return "gets";
+        }
+
+        public static string Green(this bool b)
+        {
+            if (b == true)
+                return "be";
+//  может поменять с другим блоком else
+            else
+                return " more bucks";
+        }
+    }
+}
---
+++ Part_14/665_HOLOD/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("665_HOLOD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("665_HOLOD")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a328db5e-05af-4ba6-86a5-36bf6e6abbb9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
